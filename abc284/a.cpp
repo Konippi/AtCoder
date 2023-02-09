8c40ed59b666cf470af97107e4a377edd40ce02f
Konippi <konippi-114@outlook.jp>
2023-02-09 23:22:51 +0900

    added a.cpp

---
+++ abc284/a.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+using Graph = vector<vector<int>>;
+typedef long long ll;
+ll MOD = 998244353;
+ll INF = 1LL << 60;
+double PI = 3.14159265359;
+
+int main(void) {
+    int n;
+    cin >> n;
+    vector<string> s(n);
+    for (int i = 0; i < n; i++) {
+        cin >> s[i];
+    }
+    reverse(s.begin(), s.end());
+    for (auto a : s) {
+        cout << a << endl;
+    }
+
+    return 0;
+}
